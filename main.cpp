39f72f0a29d2f01a9f6422d94e38817133d98a9a
IntuitiveHarmon <echo@ununique3.com>
2022-12-29 04:08:30 +0000

    Part 1c - 10th UDT

--- main.cpp
+++ main.cpp
@@ -376,17 +376,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) Bicycle
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) Gears
+    2) Frame
+    3) Wheel
+    4) Brakes
+    5) Handlebars
 3 things it can do:
-    1)
-    2)
-    3)
+    1) shift gears
+    2) pedal forward
+    3) brake
  */
 
  /*
